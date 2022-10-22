cad266c62698aa96aa8cedad854f3ac73ddecf97
Alisa <petrunina.alisa@gmail.com>
2022-10-22 21:08:40 +0300

    less3

---
+++ Lesson3/new2.js
@@ -0,0 +1,15 @@
+let age = prompt('Введите число');
+switch (age) {
+    case '18':
+        alert("Отлично!");
+        break;
+    case '10':
+        alert("Not enough");
+        break
+    case '30': 
+        alert("Go to sleep");
+        break;
+    default:
+        alert("Мы не знаем, что Вам делать :)")
+
+}
